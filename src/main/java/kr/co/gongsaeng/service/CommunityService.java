872df92d969d73b0ed3d8d1b15c9ee3cf398bf98
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2024-02-02 19:16:29 +0900

    fix : 커뮤니티(고은) - 0202

--- src/main/java/kr/co/gongsaeng/service/CommunityService.java
+++ src/main/java/kr/co/gongsaeng/service/CommunityService.java
@@ -1,19 +1,19 @@
-package kr.co.gongsaeng.service;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import kr.co.gongsaeng.mapper.BoardMapper;
-import kr.co.gongsaeng.vo.BoardVO;
-
-@Service
-public class CommunityService {
-	@Autowired
-	private BoardMapper mapper;
-	
-	// 게시물 등록 요청
-	public int registBoard(BoardVO board) {
-		return mapper.insertBoard(board);
-	}
-
-}
+//package kr.co.gongsaeng.service;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import kr.co.gongsaeng.mapper.BoardMapper;
+//import kr.co.gongsaeng.vo.BoardVO;
+//
+//@Service
+//public class CommunityService {
+//	@Autowired
+//	private BoardMapper mapper;
+//	
+//	// 게시물 등록 요청
+//	public int registBoard(BoardVO board) {
+//		return mapper.insertBoard(board);
+//	}
+//
+//}
