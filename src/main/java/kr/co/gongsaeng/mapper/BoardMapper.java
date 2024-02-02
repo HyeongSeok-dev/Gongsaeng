963753b6e635bcd46155d485294df59bc0e0de95
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2024-02-02 19:10:26 +0900

    feat: 커뮤니티 (고은) - 0202

--- src/main/java/kr/co/gongsaeng/mapper/BoardMapper.java
+++ src/main/java/kr/co/gongsaeng/mapper/BoardMapper.java
@@ -1,12 +1,9 @@
 package kr.co.gongsaeng.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
-
 import kr.co.gongsaeng.vo.BoardVO;
 
-@Mapper
-public class BoardMapper {
-//	int insertBoard(BoardVO board);
+public interface BoardMapper {
 
+	int insertBoard(BoardVO board);
 
 }
