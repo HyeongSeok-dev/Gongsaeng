ac6dd357f61eb56c02a98a994611ac6c31d0ee4a
Goeun <Goeun@DESKTOP-MQFR7EI>
2024-02-08 12:45:29 +0900

    feat: 함께해요(고은) - 0208

--- src/main/java/kr/co/gongsaeng/service/CommunityService.java
+++ src/main/java/kr/co/gongsaeng/service/CommunityService.java
@@ -17,15 +17,16 @@ public class CommunityService {
 	public int registBoard(BoardVO board) {
 		return mapper.insertBoard(board);
 	}
-
-	// 게시물 목록 조회 요청
-	public List<BoardVO> getBoardList(String searchType, String searchKeyword, int startRow, int listLimit) {
-		return mapper.selectBoardList(searchType, searchKeyword, startRow, listLimit);
+	
+	// 함께해요 게시물 목록 조회 요청
+	public List<BoardVO> getTogetherList(String sId, int startRow, int listLimit) {
+		return mapper.selectTogetherList(sId, startRow, listLimit);
 	}
 	
-	// 전체 게시물 목록 갯수 조회 요청
-	public int getBoardListCount(String searchType, String searchKeyword) {
-		return mapper.selectBoardListCount(searchType, searchKeyword);
+	// 함께해요 게시물 목록 개수 조회 요청
+	public int getTogetherListCount(String sId) {
+		return mapper.selectTogetherListCount(sId);
 	}
+
 
 }
